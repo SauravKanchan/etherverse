3b62cdb9339b4db0cda26be011829c6613dfca14
SauravKanchan <sauravnk30@gmail.com>
2022-12-02 21:33:28 +0530

    add building

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,7 @@
 <script>
   import kaboom from "kaboom"
+  import { setCharAt, setOnMap } from "./utils"
+  
 
   const DIMENSION = {
     x: window.innerWidth,
@@ -19,6 +21,10 @@
 
   loadRoot("assets/")
   loadSprite("grass","grass.png")
+  loadSprite("bb", "building_bottom.png")
+  loadSprite("mb", "building_middle.png")
+  loadSprite("door", "door.png")
+  loadSprite("window", "window.png")
   loadSpriteAtlas("player.png", "player.json")
 
 
@@ -26,22 +32,48 @@
     layers(["bg", "obj", "ui"], "obj")
   })
 
-  const tile = {width: 12, height: 12}
+  const tile = {width: 16, height: 16}
   let map = []
   let row = ''
-  for(let x=0; x < DIMENSION.x/(tile.width*SCALE); x++){
+  for(let x=0; x < DIMENSION.x/(tile.width); x++){
     row += ' '
   }
-  console.log({row})
-  for(let y=0; y <  DIMENSION.y/(tile.width*SCALE); y++){
+  for(let y=0; y <  DIMENSION.y/(tile.width); y++){
     map.push(row)
   }
-  console.log(map)
+
+  // Making Building
+  const BUILDING_START = {x: 10, y: 5}
+  const BUILDING_DIMENSION = {width: 11, height: 10};
+  for(let x=0; x< BUILDING_DIMENSION.width; x++){
+    for(let y=0; y< BUILDING_DIMENSION.height; y++){
+      map = setOnMap(map, BUILDING_START.x + x, BUILDING_START.y+y, "#")
+    }
+  }
+  for(let x=0; x< BUILDING_DIMENSION.width; x++){
+      map = setOnMap(map, BUILDING_START.x + x, BUILDING_START.y+BUILDING_DIMENSION.height, "-")
+  }
+  setOnMap(map, BUILDING_START.x+Math.floor(BUILDING_DIMENSION.width/2), BUILDING_START.y+BUILDING_DIMENSION.height, "!") 
+  setOnMap(map, BUILDING_START.x+Math.floor(BUILDING_DIMENSION.width/2)-3, BUILDING_START.y+BUILDING_DIMENSION.height-2, "w") 
+  setOnMap(map, BUILDING_START.x+Math.floor(BUILDING_DIMENSION.width/2)+3, BUILDING_START.y+BUILDING_DIMENSION.height-2, "w") 
+
+  setOnMap(map, BUILDING_START.x+Math.floor(BUILDING_DIMENSION.width/2)-3, BUILDING_START.y+BUILDING_DIMENSION.height-4, "w") 
+  setOnMap(map, BUILDING_START.x+Math.floor(BUILDING_DIMENSION.width/2)+3, BUILDING_START.y+BUILDING_DIMENSION.height-4, "w") 
+
+  setOnMap(map, BUILDING_START.x+Math.floor(BUILDING_DIMENSION.width/2)-3, BUILDING_START.y+BUILDING_DIMENSION.height-6, "w") 
+  setOnMap(map, BUILDING_START.x+Math.floor(BUILDING_DIMENSION.width/2)+3, BUILDING_START.y+BUILDING_DIMENSION.height-6, "w") 
+
+  setOnMap(map, BUILDING_START.x+Math.floor(BUILDING_DIMENSION.width/2)-3, BUILDING_START.y+BUILDING_DIMENSION.height-8, "w") 
+  setOnMap(map, BUILDING_START.x+Math.floor(BUILDING_DIMENSION.width/2)+3, BUILDING_START.y+BUILDING_DIMENSION.height-8, "w") 
 
   const levelCfg = {
     width: tile.width,
     height: tile.height,
-    ' ': () => [sprite('grass')],
+    ' ': () => [sprite('grass'), scale(1.25),layer("bg") ],
+    '#': () => [sprite('mb'), area(),solid(),"build2"],
+    '-': () => [sprite('bb'), area(),solid()],
+    '!': () => [sprite('door')],
+    'w': () => [sprite('window')]
   }
   
   const gameLevel = addLevel(map, levelCfg)
@@ -49,45 +81,56 @@
   const player = add([
     sprite("hero"),
     pos(DIMENSION.x/(SCALE*2), DIMENSION.y/(SCALE*2)),
-    scale(0.5)
+    scale(0.5),
+    layer("obj"),
+    area(),
+    solid()
   ])
 
   player.play("idle-down")
+
+
+  onCollide("player", "build2", ()=> {
+    console.log("its colliding");
+  })
+
 
   player.action(() => {
     const left = keyIsDown('left')
     const right = keyIsDown('right')
     const up = keyIsDown('up')
     const down = keyIsDown('down')
-    const speed = 20/SCALE
+    const speed = 200
+    
     const currAnim = player.curAnim()
     // var currCam = camPos();
-    camPos(player.pos.add(DIMENSION.x/(SCALE*2),DIMENSION.y/(SCALE*2)))
-    // camPos(player.pos)
-  
+      camPos(player.pos.add(DIMENSION.x/(SCALE*2),DIMENSION.y/(SCALE*2)))
+
 
     if (left) {
         if(currAnim !== 'move-left') {
           player.play('move-left')
         }
-        player.pos.x -= speed
+        player.move(-speed, 0)
     } else if (right) {
       if(currAnim !== 'move-right') {
           player.play('move-right')
       }
-      player.pos.x += speed
+      player.move(speed, 0)
     } else if (up) {
         if(currAnim !== 'move-up') {
           player.play('move-up')
         }
-        player.pos.y -= speed
+        player.move(0,-speed)
     } else if (down) {
       if(currAnim !== 'move-down') {
           player.play('move-down')
       }
-      player.pos.y += speed
+      player.move(0,speed)
     }
 
   })
   
+  debug.inspect = true
+
 </script>
