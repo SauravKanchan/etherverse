f8506000d2026d7d31ec86853327e0aad48c683f
SauravKanchan <sauravnk30@gmail.com>
2022-12-02 14:56:33 +0530

    intial commit

---
+++ src/App.svelte
@@ -0,0 +1,66 @@
+<script>
+  import kaboom from "kaboom"
+
+  kaboom({
+    global: true,
+    scale: 2,
+    debug: true,
+    background: [0,0,0,1],
+  })
+
+  loadRoot("assets/")
+  loadSprite("grass","grass.png")
+
+
+  scene("game", () => {
+    layers(["bg", "obj", "ui"], "obj")
+  })
+
+
+  const map = [
+    '                                                                     ',
+    '                                                                     ',
+    '                                                                     ',
+    '                                                                     ',
+    '                                                                     ',
+    '                                                                     ',
+    '                                                                     ',
+    '                                                                     ',
+    '                                                                     ',
+    '                                                                     ',
+    '                                                                     ',
+    '                                                                     ',
+    '                                                                     ',
+    '                                                                     ',
+    '                                                                     ',
+    '                                                                     ',
+    '                                                                     ',
+    '                                                                     ',
+    '                                                                     ',
+    '                                                                     ',
+    '                                                                     ',
+    '                                                                     ',
+    '                                                                     ',
+    '                                                                     ',
+    '                                                                     ',
+    '                                                                     ',
+    '                                                                     ',
+    '                                                                     ',
+    '                                                                     ',
+    '                                                                     ',
+    '                                                                     ',
+    '                                                                     ',
+    '                                                                     ',
+    '                                                                     ',
+    '                                                                     ',
+  ]
+
+  const levelCfg = {
+    width: 12,
+    height: 12,
+    ' ': () => [sprite('grass')],
+  }
+  
+  const gameLevel = addLevel(map, levelCfg)
+
+</script>
