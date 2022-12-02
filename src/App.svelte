8fadaa302ff06135ae37aa2e4878a859e6eaa3a7
SauravKanchan <sauravnk30@gmail.com>
2022-12-02 17:51:22 +0530

    character moving

--- src/App.svelte
+++ src/App.svelte
@@ -10,6 +10,7 @@
 
   loadRoot("assets/")
   loadSprite("grass","grass.png")
+  loadSpriteAtlas("player.png", "player.json")
 
 
   scene("game", () => {
@@ -63,4 +64,47 @@
   
   const gameLevel = addLevel(map, levelCfg)
 
+  const player = add([
+    sprite("hero"),
+    pos(100,100)
+  ])
+
+  let i = 0;
+  player.play("idle-down")
+
+  player.action(() => {
+    const left = keyIsDown('left')
+    const right = keyIsDown('right')
+    const up = keyIsDown('up')
+    const down = keyIsDown('down')
+    const speed = 10
+    const currAnim = player.curAnim()
+    var currCam = camPos();
+    camPos(player.pos);
+  
+
+    if (left) {
+        if(currAnim !== 'move-left') {
+          player.play('move-left')
+        }
+        player.pos.x -= speed
+    } else if (right) {
+      if(currAnim !== 'move-right') {
+          player.play('move-right')
+      }
+      player.pos.x += speed
+    } else if (up) {
+        if(currAnim !== 'move-up') {
+          player.play('move-up')
+        }
+        player.pos.y -= speed
+    } else if (down) {
+      if(currAnim !== 'move-down') {
+          player.play('move-down')
+      }
+      player.pos.y += speed
+    }
+
+  })
+  
 </script>
